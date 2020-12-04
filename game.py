f558a17469856854d3097b83f19a12eae831ba34
3rdWindBC <69737771+3rdWindBC@users.noreply.github.com>
2020-12-04 14:16:11 -0800

    Step 14 completed

--- game.py
+++ game.py
@@ -19,17 +19,23 @@ class Game:
 
     def start(self):
         self.welcome()
-        print(f"Number Missed: {self.missed}")
-        self.active_phrase.display(self.guesses)
-        user_guess = self.get_guess()
-        self.guesses.append(user_guess)
-        if self.active_phrase.check_guess(user_guess):
-            print("YAY")
-        else:
-            print("Bummer!")
-
-
-
+        while self.missed < 5 and self.active_phrase.check_complete(self.guesses) is False:
+            print(f"Number Missed: {self.missed}")
+            self.active_phrase.display(self.guesses)
+            user_guess = self.get_guess()
+            self.guesses.append(user_guess)
+            if not self.active_phrase.check_guess(user_guess):
+                self.missed += 1
+        self.game_over()
+            
     def get_guess(self):
         letter = input("\n\nPlease enter a letter: ")
         return letter
+
+    def game_over(self):
+        if self.missed == int(5):
+            print("Sorry, you lose. GAME OVER")
+        else:
+            print("Great job, YOU WIN!!")
+
+
